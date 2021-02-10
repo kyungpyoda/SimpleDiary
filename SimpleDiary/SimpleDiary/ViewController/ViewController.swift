3a2479792bdd3e6cbb854ffb80b2076df9074968
kyungpyoda <qorghdl@naver.com>
2021-02-10 22:12:41 +0900

    메인 뷰컨 테이블 뷰 기본 구성

---
+++ SimpleDiary/SimpleDiary/ViewController/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SimpleDiary
+//
+//  Created by 홍경표 on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var data: [String] = []
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+
+    private func setUp() {
+        data = ["abc", "가나다", "1234"]
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MemoTableViewCell", bundle: nil), forCellReuseIdentifier: MemoTableViewCell.identifier)
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MemoTableViewCell.identifier, for: indexPath) as? MemoTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(content: data[indexPath.row])
+        return cell
+    }
+}
